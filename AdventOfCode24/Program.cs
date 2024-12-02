3d9b6588f1a3728c7a08958dfc6abec6ec998f51
Sami Lappalainen <lappalainen.sami@outlook.com>
2024-12-02 13:41:52 +0100

    Day 1 Part 2

--- AdventOfCode24/Program.cs
+++ AdventOfCode24/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Day1());
+        Console.WriteLine(Day1Part2());
     }
 
     static int Day1()
@@ -31,5 +31,32 @@ class Program
         }
 
         return sumOfDistances;
+    }
+
+    static int Day1Part2()
+    {
+        string[] input = File.ReadAllLines("../../../Day1Input.txt");
+
+        int[] leftValues = new int[input.Length];
+        int[] rightValues = new int[input.Length];
+        
+        for (int i = 0; i < input.Length; i++)
+        {
+            leftValues[i] = int.Parse(input[i].Split()[0]);
+            rightValues[i] = int.Parse(input[i].Split()[^1]);
+        }
+        
+        Array.Sort(leftValues);
+        Array.Sort(rightValues);
+
+        int similarityScore = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            int n = leftValues[i];
+            similarityScore += n * rightValues.Count(j => j == n);
+        }
+
+        return similarityScore;
     }
 }
